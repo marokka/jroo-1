21329a4e478c2b7af68633c73921189aa043af5b
marokka <yaltakanc@gmail.com>
2019-09-17 10:45:28 +0300

    Shirokov

--- resources/views/static/contact.blade.php
+++ resources/views/static/contact.blade.php
@@ -5,6 +5,18 @@
  * Date: 10.09.2019
  * Time: 20:40
  */
+ ?>
+
+@extends('layout.frontend')
+
+@section('content')
+    {{ Widget::run('breadcumb.breadcumbWidget', ['items' => [
+                \App\Widgets\Breadcumb\models\Breadcrumb::create('Основной сайт', '#', 'fas fa-home'),
+                \App\Widgets\Breadcumb\models\Breadcrumb::create('Категории', route('home')),
+                \App\Widgets\Breadcumb\models\Breadcrumb::create($breadcumb, '#'),
+            ]])
+    }}
+
 <!-- End breadcrumb-->
 <div class="contact-us">
   <div class="container">
@@ -57,4 +69,5 @@
   </div>
 </div>
 <!-- End contact us-->
-?>
+
+@endsection
