176a76356934ad9665bc95fedb24577894ba3b6d
marokka <yaltakanc@gmail.com>
2019-09-18 23:41:50 +0300

    Shirokov

--- resources/views/static/delivery.blade.php
+++ resources/views/static/delivery.blade.php
@@ -5,3 +5,16 @@
  * Date: 10.09.2019
  * Time: 20:40
  */
+?>
+@extends('layout.frontend')
+
+@section('content')
+    {{ Widget::run('breadcumb.breadcumbWidget', ['items' => [
+                \App\Widgets\Breadcumb\models\Breadcrumb::create('Основной сайт', '#', 'fas fa-home'),
+                \App\Widgets\Breadcumb\models\Breadcrumb::create('Доставка', '#'),
+            ]])
+        }}
+
+ 
+
+@endsection
