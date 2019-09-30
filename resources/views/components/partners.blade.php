36b50d90cc99d0283ee191389757e801569f11d3
marokka <yaltakanc@gmail.com>
2019-09-30 15:13:11 +0300

    Настроил ссылки на партнерах

--- resources/views/components/partners.blade.php
+++ resources/views/components/partners.blade.php
@@ -1,19 +1,19 @@
 <div class="partner">
     <div class="container">
         <div class="partner_block d-flex justify-content-between" data-slick="{&quot;slidesToShow&quot;: 6}">
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/123.png')}}"
+            <div class="partner--logo" href=""><a href="https://ms-kids.com/" target="_blank"><img src="{{asset('frontend/images/partner/123.png')}}"
                                                                 alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/124.png')}}"
+            <div class="partner--logo" href=""><a href="https://www.gussi-gussi.com/" target="_blank"><img src="{{asset('frontend/images/partner/124.png')}}"
                                                                 alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/125.png')}}"
+            <div class="partner--logo" href=""><a href="http://www.cc-geneva.cc/" target="_blank"><img src="{{asset('frontend/images/partner/125.png')}}"
                                                                 alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/126.png')}}"
+            <div class="partner--logo" href=""><a href="https://www.instagram.com/alkobazar/?utm_source=ig_profile_share&igshid=6lqpwaryw0we" target="_blank"><img src="{{asset('frontend/images/partner/126.png')}}"
                                                                 alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/127.png')}}"
+            <div class="partner--logo" href=""><a href="https://www.instagram.com/atlas_sip/?utm_source=ig_profile_share&igshid=16kr9puif8kiq" target="_blank"><img src="{{asset('frontend/images/partner/127.png')}}"
                                                                 alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/128.png')}}"
+            <div class="partner--logo" href=""><a href="#" target="_blank"><img src="{{asset('frontend/images/partner/128.png')}}"
                                                                 alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#"><img src="{{asset('frontend/images/partner/129.png')}}"
+            <div class="partner--logo" href=""><a href="#" target="_blank"><img src="{{asset('frontend/images/partner/129.png')}}"
                                                                 alt="partner logo"></a></div>
         </div>
     </div>
