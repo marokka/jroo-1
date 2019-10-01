399a59a96c2e8d4383388fd9956fc00d8511fe52
marokka <yaltakanc@gmail.com>
2019-10-01 13:25:15 +0300

    Shirokov

--- resources/views/layout/frontend.blade.php
+++ resources/views/layout/frontend.blade.php
@@ -34,15 +34,11 @@
                     <div class="col-12 col-md-4">
                         <div
                             class="header-right d-flex justify-content-around">
-                            <div class="social-link d-flex"><a href="https://vk.com/club136274972" target="_blank"><img
-                                        src="{{asset('frontend/images/icon/vk-com.png')}}"
-                                        style="width: 3em"> </a><a
-                                    href="https://www.facebook.com/jrooburgersteak/" target="_blank">
-                                    <img src="{{asset('frontend/images/icon/facebook-new.png')}}"
-                                         style="width: 3em"></a><a href="https://www.instagram.com/jroo_burger_steak/"
-                                                                   target="_blank"><img
-                                        src="{{asset('frontend/images/icon/instagram.png')}}" style="width: 3em">
-                                </a>
+                            <div class="social-link d-flex">
+                                <a href="https://vk.com/club136274972" target="_blank"><img src="{{asset('frontend/images/icon/vk-com.png')}}" style="width: 3em"> </a>
+                                <a href="https://www.facebook.com/jrooburgersteak/" target="_blank"><img src="{{asset('frontend/images/icon/facebook-new.png')}}" style="width: 3em"></a>
+                                <a href="https://www.instagram.com/jroo_burger_steak/" target="_blank"><img src="{{asset('frontend/images/icon/instagram.png')}}" style="width: 3em"></a>
+                                <a href="https://www.tripadvisor.ru/UserReviewEdit-g295378-d13001575-Jroo_Burger_Steak-Yalta.html" target="_blank"><img src="{{asset('frontend/images/icon/tripadvisor.png')}}" style="width: 3em"></a>
                             </div>
 
                             @guest
