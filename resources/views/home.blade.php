438a2cb9e454c4ac7b212cfe692d52e2a9f8c8e2
marokka <yaltakanc@gmail.com>
2019-10-21 16:20:00 +0300

    Shirokov

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,50 +7,43 @@
 @extends('layout.frontend')
 
 @section('content')
-{{--    {{ Widget::run('breadcumb.breadcumbWidget', ['items' => [--}}
-{{--            \App\Widgets\Breadcumb\models\Breadcrumb::create('Главная', '/', 'fas fa-home'),--}}
-{{--            \App\Widgets\Breadcumb\models\Breadcrumb::create('Категории', '#'),--}}
-{{--        ]])--}}
-{{--    }}--}}
-{{--    <div class="shop-layout">--}}
-{{--        <div class="container">--}}
-{{--            @if(Session::has('message'))--}}
-{{--                <div class="alert {{Session::get('message')['class']}}">--}}
-{{--                    {{Session::get('message')['message']}}--}}
-{{--                </div>--}}
-{{--            @endif--}}
-{{--            <div class="ogami-container-fluid">--}}
-{{--                <div class="slider-banner">--}}
-{{--                    <div class="row">--}}
-{{--                        @foreach($categories as $category)--}}
-{{--                            <div class="col-12 col-sm-4 col-lg-4">--}}
-{{--                                <a href="{{route('food.by-category-slug', $category->slug)}}">--}}
-{{--                                    <img class="img-fluid" src="{{$category->img}}"--}}
-{{--                                         alt="{{$category->name}}" style="margin-bottom: 8%; border-radius: 14px;"--}}
-{{--                                         title="{{$category->name}}">--}}
-{{--                                </a>--}}
-{{--                            </div>--}}
-{{--                        @endforeach--}}
+    {{ Widget::run('breadcumb.breadcumbWidget', ['items' => [
+            \App\Widgets\Breadcumb\models\Breadcrumb::create('Главная', '/', 'fas fa-home'),
+            \App\Widgets\Breadcumb\models\Breadcrumb::create('Категории', '#'),
+        ]])
+    }}
+    <div class="shop-layout">
+        <div class="container">
+            @if(Session::has('message'))
+                <div class="alert {{Session::get('message')['class']}}">
+                    {{Session::get('message')['message']}}
+                </div>
+            @endif
+            <div class="ogami-container-fluid">
+                <div class="slider-banner">
+                    <div class="row">
+                        @foreach($categories as $category)
+                            <div class="col-12 col-sm-4 col-lg-4">
+                                <a href="{{route('food.by-category-slug', $category->slug)}}">
+                                    <img class="img-fluid" src="{{$category->img}}"
+                                         alt="{{$category->name}}" style="margin-bottom: 8%; border-radius: 14px;"
+                                         title="{{$category->name}}">
+                                </a>
+                            </div>
+                        @endforeach
 
-{{--                        <div class="col-12 col-sm-4 col-lg-4">--}}
-{{--                            <a href="{{route('foods.index')}}">--}}
-{{--                                <img class="img-fluid"--}}
-{{--                                     src="{{asset('frontend/images/categories/other.jpg')}}"--}}
-{{--                                     alt="Все блюда" style="margin-bottom: 8%; border-radius: 14px;" title="Все блюда">--}}
-{{--                            </a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-<div class="coming-soon">
-    <div class="container">
-        <div class="coming-soon_block" style="    width: 60%; background: white; border-radius: 10px;padding: 7px; margin-left: 20%;">
-            <h1 class="title">Jroo-доставка</h1>
-            <h5 class="subtitle" style="margin-bottom: 0px !important;">Данный ресурс ожидает оплаты</h5>
+                        <div class="col-12 col-sm-4 col-lg-4">
+                            <a href="{{route('foods.index')}}">
+                                <img class="img-fluid"
+                                     src="{{asset('frontend/images/categories/other.jpg')}}"
+                                     alt="Все блюда" style="margin-bottom: 8%; border-radius: 14px;" title="Все блюда">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
+
     <!-- End shop layout-->
 @endsection
