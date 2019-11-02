18c9ac913850c1341368ccc06631c4d00a2f0776
marokka <yaltakanc@gmail.com>
2019-11-02 12:45:15 +0300

    Shirokov

--- resources/views/components/partners.blade.php
+++ resources/views/components/partners.blade.php
@@ -1,19 +1,17 @@
 <div class="partner">
     <div class="container">
-        <div class="partner_block d-flex justify-content-between" data-slick="{&quot;slidesToShow&quot;: 6}">
-            <div class="partner--logo" href=""><a href="https://ms-kids.com/" target="_blank"><img src="{{asset('frontend/images/partner/123.png')}}"
-                                                                alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="https://www.gussi-gussi.com/" target="_blank"><img src="{{asset('frontend/images/partner/124.png')}}"
-                                                                alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="http://www.cc-geneva.cc/" target="_blank"><img src="{{asset('frontend/images/partner/125.png')}}"
-                                                                alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="https://www.instagram.com/alkobazar/?utm_source=ig_profile_share&igshid=6lqpwaryw0we" target="_blank"><img src="{{asset('frontend/images/partner/126.png')}}"
-                                                                alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="https://www.instagram.com/atlas_sip/?utm_source=ig_profile_share&igshid=16kr9puif8kiq" target="_blank"><img src="{{asset('frontend/images/partner/127.png')}}"
-                                                                alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#" target="_blank"><img src="{{asset('frontend/images/partner/128.png')}}"
-                                                                alt="partner logo"></a></div>
-            <div class="partner--logo" href=""><a href="#" target="_blank"><img src="{{asset('frontend/images/partner/129.png')}}"
-                                                                alt="partner logo"></a></div>
+        <div class="partner_block d-flex justify-content-between" data-slick="{&quot;slidesToShow&quot;: 7}">
+            <div class="partner--logo" ><a href="https://ms-kids.com/" target="_blank"><img src="{{asset('frontend/images/partner/123.png')}}"
+                                                                                            alt="магазин Magic Store — партнёр GUSSI Доставка"></a></div>
+            <div class="partner--logo" ><a href="https://www.gussi-gussi.com/" target="_blank"><img src="{{asset('frontend/images/partner/124.png')}}"
+                                                                                                    alt="ресторан Gussi — партнёр GUSSI Доставка"></a></div>
+            <div class="partner--logo" ><a href="http://www.cc-geneva.cc/" target="_blank"><img src="{{asset('frontend/images/partner/125.png')}}"
+                                                                                                alt="Ресторан Geneva — партнёр GUSSI Доставка"></a></div>
+            <div class="partner--logo" ><a href="https://www.instagram.com/alkobazar/?utm_source=ig_profile_share&igshid=6lqpwaryw0we" target="_blank"><img src="{{asset('frontend/images/partner/126.png')}}"
+                                                                                                                                                            alt="алкобазар Ящик — партнёр GUSSI Доставка"></a></div>
+            <div class="partner--logo" ><a href="https://www.instagram.com/atlas_sip/?utm_source=ig_profile_share&igshid=16kr9puif8kiq" target="_blank"><img src="{{asset('frontend/images/partner/127.png')}}"
+                                                                                                                                                             alt="кафе Atlas — партнёр GUSSI Доставка"></a></div>
+            <div class="partner--logo"><a href="http://jroo.cafe/" target="_blank"><img src="{{asset('frontend/images/partner/200.png')}}" alt="Ресторан Jroo — партнёр GUSSI Доставка"></a></div>
+            <div class="partner--logo"><a href="http://dap.group/" target="_blank"><img src="{{asset('frontend/images/partner/201.png')}}" alt="DAP — партнёр GUSSI Доставка"></a></div>
         </div>
     </div>
