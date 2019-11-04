9ea5aa8db4865d3c788c6dae471dcd87e8041fed
marokka <yaltakanc@gmail.com>
2019-11-04 03:00:00 +0300

    Update social_network.blade.php

--- resources/views/components/social_network.blade.php
+++ resources/views/components/social_network.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-12 col-sm-4">
             <div class="text-center">
-                <iframe src="https://averin.pro/widget.php?l=jroo_burger_steak&style=4&width=250&gallery=1&s=80&icc=3&icr=3&t=1&tt=Мы в Инстаграм&h=1&ttcolor=FFFFFF&th=c3c3c3&bw=f9f9f9&bscolor=FFFFFF&bs=FF0000&ts=Подписаться"allowtransparency="true" frameborder="0" scrolling="no"style="border:none;overflow:hidden;width:auto; height: 490px"></iframe>
+                <iframe src="https://averin.pro/widget.php?l=jroo_burger_steak&style=1&width=250&gallery=1&s=80&icc=3&icr=3&t=1&tt=#jroo_burger_steak&h=1&ttcolor=FFFFFF&th=c3c3c3&bw=ffffff&bscolor=FFFFFF&bs=FF0000&ts=Подписаться" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:100%; height: 490px" ></iframe>
             </div>
         </div>
         <div class="col-12 col-sm-4">
