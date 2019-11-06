af74cba2787d58a0e711d07eccc0399717762b9e
marokka <yaltakanc@gmail.com>
2019-11-07 02:35:04 +0300

    Update story.blade.php

--- resources/views/components/story.blade.php
+++ resources/views/components/story.blade.php
@@ -62,88 +62,88 @@
                 </div>
             </div>
         </div>
-        <!-- End introduce-->
-        <div class="delivery-process">
-            <div class="benefit-block">
-                <div class="container">
-                    <div class="our-benefits">
-                        <div class="row">
-                            <div class="col-12 col-md-4">
-                                <div class="benefit-detail d-flex flex-column align-items-center"><img class="benefit-img" src="{{asset('frontend/images/icon-item/shipped.png')}}" alt="">
-                                    <h5 class="benefit-title">Free Shipping</h5>
-                                    <p class="benefit-describle">For all order over 99$</p>
-                                </div>
-                            </div>
-                            <div class="col-12 col-md-4">
-                                <div class="benefit-detail d-flex flex-column align-items-center"><img class="benefit-img" src="{{asset('frontend/images/icon-item/time.png')}}" alt="">
-                                    <h5 class="benefit-title">Delivery On Time</h5>
-                                    <p class="benefit-describle">If good have prolems</p>
-                                </div>
-                            </div>
-                            <div class="col-12 col-md-4">
-                                <div class="benefit-detail boderless d-flex flex-column align-items-center"><img class="benefit-img" src="{{asset('frontend/images/icon-item/money.png')}}" alt="">
-                                    <h5 class="benefit-title">Secure Payment</h5>
-                                    <p class="benefit-describle">100% secure payment</p>
-                                </div>
-                            </div>
+    </div>
+</div>
+<!-- End introduce-->
+<div class="delivery-process">
+    <div class="benefit-block">
+        <div class="container">
+            <div class="our-benefits">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <div class="benefit-detail d-flex flex-column align-items-center"><img class="benefit-img" src="{{asset('frontend/images/icon-item/shipped.png')}}" alt="">
+                            <h5 class="benefit-title">Free Shipping</h5>
+                            <p class="benefit-describle">For all order over 99$</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="benefit-detail d-flex flex-column align-items-center"><img class="benefit-img" src="{{asset('frontend/images/icon-item/time.png')}}" alt="">
+                            <h5 class="benefit-title">Delivery On Time</h5>
+                            <p class="benefit-describle">If good have prolems</p>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4">
+                        <div class="benefit-detail boderless d-flex flex-column align-items-center"><img class="benefit-img" src="{{asset('frontend/images/icon-item/money.png')}}" alt="">
+                            <h5 class="benefit-title">Secure Payment</h5>
+                            <p class="benefit-describle">100% secure payment</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="delivery-process_holder">
-                <div class="full-fluid">
-                    <div class="container">
-                        <div class="delivery-process_wrapper text-center">
-                            <h1 class="title coffee-underline text-white">Delivery Process</h1>
-                            <div class="delivery-process_block">
-                                <div class="row justify-content-between">
-                                    <div class="col-6 col-md-3">
-                                        <div class="process">
-                                            <div class="process-icon"><img src="{{asset('frontend/images/icon-item/item11.png')}}" alt=""></div>
-                                            <div class="process-text">
-                                                <h5>Step 1</h5>
-                                                <p>Choose Your Products</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-3">
-                                        <div class="process">
-                                            <div class="process-icon"><img src="{{asset('frontend/images/icon-item/item12.png')}}" alt=""></div>
-                                            <div class="process-text">
-                                                <h5>Step 2</h5>
-                                                <p>Connect Nearest Farm</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-3">
-                                        <div class="process">
-                                            <div class="process-icon"><img src="{{asset('frontend/images/icon-item/item13.png')}}" alt=""></div>
-                                            <div class="process-text">
-                                                <h5>Step 3</h5>
-                                                <p>Share Your Location</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-6 col-md-3">
-                                        <div class="process">
-                                            <div class="process-icon clear"><img src="{{asset('frontend/images/icon-item/item14.png')}}"alt=""></div>
-                                            <div class="process-text">
-                                                <h5>Step 4</h5>
-                                                <p>Get Delivered Fast</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End delivery-process-->
-        <div class="col-12 text-center" style="background-color: #2C150C; border-radius: 10px; padding: 20px;">
-            <h1 style="font-size: 1.3em; color: white;font-weight: bold;">JROO burger & steak включает в себя простоту и локаничность интерьера, безупречный вкус качественных местных продуктов и доступную стоимость блюд.
-                Мы удивим Вас простотой исполнения и в то же время изяществом вкуса сочных стейков, приготовленных из мяса местного производства.</h1>
         </div>
     </div>
+    <div class="delivery-process_holder">
+        <div class="full-fluid">
+            <div class="container">
+                <div class="delivery-process_wrapper text-center">
+                    <h1 class="title coffee-underline text-white">Delivery Process</h1>
+                    <div class="delivery-process_block">
+                        <div class="row justify-content-between">
+                            <div class="col-6 col-md-3">
+                                <div class="process">
+                                    <div class="process-icon"><img src="{{asset('frontend/images/icon-item/item11.png')}}" alt=""></div>
+                                    <div class="process-text">
+                                        <h5>Step 1</h5>
+                                        <p>Choose Your Products</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="process">
+                                    <div class="process-icon"><img src="{{asset('frontend/images/icon-item/item12.png')}}" alt=""></div>
+                                    <div class="process-text">
+                                        <h5>Step 2</h5>
+                                        <p>Connect Nearest Farm</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="process">
+                                    <div class="process-icon"><img src="{{asset('frontend/images/icon-item/item13.png')}}" alt=""></div>
+                                    <div class="process-text">
+                                        <h5>Step 3</h5>
+                                        <p>Share Your Location</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="process">
+                                    <div class="process-icon clear"><img src="{{asset('frontend/images/icon-item/item14.png')}}"alt=""></div>
+                                    <div class="process-text">
+                                        <h5>Step 4</h5>
+                                        <p>Get Delivered Fast</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End delivery-process-->
+<div class="col-12 text-center" style="background-color: #2C150C; border-radius: 10px; padding: 20px;">
+    <h1 style="font-size: 1.3em; color: white;font-weight: bold;">JROO burger & steak включает в себя простоту и локаничность интерьера, безупречный вкус качественных местных продуктов и доступную стоимость блюд.
+        Мы удивим Вас простотой исполнения и в то же время изяществом вкуса сочных стейков, приготовленных из мяса местного производства.</h1>
 </div>
